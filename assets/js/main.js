e9a30893d372cf3402d028f354d1d3d9cffb90b4
Malte9799 <maltem.hilpert@gmx.de>
2021-02-15 14:38:06 +0100

    _push-Puzzlemode

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,26 @@
-$(document).ready(() => {
-	// initialize page on first load
-	if (localStorage.getItem('darkmode') == "false") {
-		$("body").removeClass("active")
-		$(".dark-layer").removeClass("active")
-		$(".dark-layer-button").removeClass("active")
-	}
+// initialize page on first load
+if (localStorage.getItem('darkmode') == "false") {
+	$("body").removeClass("active")
+}
 
-	if (!this.location.hash) $("#index").addClass("current")
-	$(this.location.hash).addClass("current")
+if (!this.location.hash) $("#index").addClass("current")
+$(this.location.hash).addClass("current")
 
-	window.onhashchange = function() {
-		$(".current").removeClass("current")
-		if (!this.location.hash) $("#index").addClass("current")
-		$(this.location.hash).addClass("current")
-	}
+window.onhashchange = () => {
+	console.log(document.querySelectorAll('.current'));
+	document.getElementsByClassName('current')[0].classList.remove('current')
+	if (!this.location.hash) document.getElementById('index').classList.add('current')
+	// if (!this.location.hash) $("#index").addClass("current")
+	document.getElementById(this.location.hash).classList.add('current')
+	// $(this.location.hash).addClass("current")
+}
 
-	// switch dark/light mode
-	$(".dark-button").click(() => {
-		$("body").toggleClass("active")
-		$(".dark-layer").toggleClass("active")
-		$(".dark-layer-button").toggleClass("active")
+// switch dark/light mode
+//document.getElementByClassName("dark-button").addEventListener("click", () => {
 
-		localStorage.setItem('darkmode', $("body").hasClass("active"));
-	});
+//});
+$(".dark-button").click(() => {
+	document.body.classList.toggle("active")
+
+	localStorage.setItem('darkmode', $("body").hasClass("active"));
 });
