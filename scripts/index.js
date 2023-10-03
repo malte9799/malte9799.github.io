e1ff9da330b22c89cfd742884f65f189b3d7b547
[MΛLTE] <maltem.hilpert@gmx.de>
2023-10-03 19:30:56 +0200

    more global function and variables

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-// G
+// Global functions and variables
 window.sleep = (time) => {
 	return new Promise((resolve) => setTimeout(resolve, time));
 };
@@ -7,7 +7,10 @@ window.getUrlParams = (search = undefined) => {
 	decodeURIComponent(window.location.search).replace(/[?&]+([^=&]+)=([^&]*)/g, (m, key, value) => (params[key] = value));
 	return search ? params[search] : params;
 };
+window.templates = {};
 window.isMobile = window.matchMedia('only screen and (max-width: 760px)').matches;
+
+window.img_not_found = 'https://cdn.jsdelivr.net/gh/malte9799/cdn/fluentui-emoji/not_found.png';
 
 import '/scripts/color.js';
 import '/scripts/cursor.js';
