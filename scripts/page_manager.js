7321f5b31e00e132f4335d7e51f122ebd148210f
[MΛLTE] <maltem.hilpert@gmx.de>
2023-10-03 19:29:59 +0200

    now support async script loading

--- scripts/page_manager.js
+++ scripts/page_manager.js
@@ -1,5 +1,4 @@
 $(loadPage);
-$(window).on('hashchange', loadPage);
 
 function averageRGB(color1, color2) {
 	const regex = /rgb\((\d+), (\d+), (\d+)\)/;
@@ -25,7 +24,7 @@ function loadPage() {
 	$('#menu').removeClass('expanded');
 	$('.included').addClass('old');
 	let section_old = $('section:not(.old)');
-	$.get(`/pages/${window.getUrlParams('page') || 'index'}.html`, (res) => {
+	$.get(`/pages/${getUrlParams('page') || 'index'}.html`, (res) => {
 		let section_new = $.parseHTML(res.match(/<section.*>[\s\S]*<\/section>/gm)[0]);
 		$(section_new).addClass('fullscreen');
 		$('body').append(section_new);
@@ -33,7 +32,7 @@ function loadPage() {
 			.find('a[link]')
 			.on('click', (e) => {
 				let page = $(e.target).attr('link');
-				if ((window.getUrlParams('page') || 'index') == page) return;
+				if ((getUrlParams('page') || 'index') == page) return;
 				window.history.replaceState(null, null, '?page=' + page);
 				loadPage(page);
 			});
@@ -43,9 +42,7 @@ function loadPage() {
 		let match = res.match(/<include>[\s\S]*<\/include>/);
 		if (!match) return;
 		let includes = $.parseHTML(match[0], true);
-		$(includes)
-			.find('script')
-			.each((i, e) => $.getScript($(e).attr('src')));
+		loadScripts($(includes).find('script'));
 		$(includes)
 			.find('link')
 			.each((i, e) => {
@@ -54,24 +51,35 @@ function loadPage() {
 	});
 }
 
+function loadScripts(scripts) {
+	if (!scripts || scripts.length == 0) return;
+	if (scripts.eq(0).prop('async')) {
+		$.getScript(scripts.eq(0).attr('src')).done(() => {
+			loadScripts(scripts.slice(1));
+		});
+	} else {
+		$.getScript(scripts.eq(0).attr('src'));
+		loadScripts(scripts.slice(1));
+	}
+}
+
 function page_transition(section_old, section_new) {
 	section_old.addClass('old');
 
 	let color_old = section_old.css('background-color');
 	let color_new = section_new.css('background-color');
 	let transition_color = color_old == color_new ? changeColor(color_old) : averageRGB(color_old, color_new);
-	let circle = $.parseHTML(
-		`<div class="fullscreen" style="clip-path:circle(0% at ${window.mousePos.x}px ${window.mousePos.y}px); background-color:${transition_color}; z-index:2"></div>`
-	);
+
+	let circle = $.parseHTML(`<div class="fullscreen" style="clip-path:circle(0% at ${window.mousePos.x}px ${window.mousePos.y}px); background-color:${transition_color}; z-index:2"></div>`);
 	$('body').append(circle);
 	section_new.css('clip-path', `circle(0% at ${window.mousePos.x}px ${window.mousePos.y}px)`);
 
-	window.sleep(50).then(() => {
+	sleep(50).then(() => {
 		section_new.css('clip-path', `circle(150% at ${window.mousePos.x}px ${window.mousePos.y}px)`);
 		$(circle).css('clip-path', `circle(300% at ${window.mousePos.x}px ${window.mousePos.y}px)`);
 	});
 
-	window.sleep(600).then(() => {
+	sleep(600).then(() => {
 		section_new.css('clip-path', '');
 		section_old.remove();
 		$(circle).remove();
