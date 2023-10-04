950730ddeaff87e7bbedeb3e949243ac526900c4
[MΛLTE] <maltem.hilpert@gmx.de>
2023-10-04 09:40:57 +0200

    Fixed Filepath

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-	content: ['./index.html', './pages/*.html', './scripts/*.js'],
+	content: ['./index.html', './pages/*.{html,js}', './scripts/*.js'],
 	theme: {
 		extend: {
 			colors: {
