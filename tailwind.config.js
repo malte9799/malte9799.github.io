481a8bfc221a934f706b79020ed5a1400235a4cd
[MΛLTE] <maltem.hilpert@gmx.de>
2023-10-07 11:07:37 +0200

    New Plugins for better 3d transform

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 
+const transform = `
+translate3d(var(--tw-translate-x), var(--tw-translate-y), var(--tw-translate-z)) 
+rotateX(var(--tw-rotate-x)) rotateY(var(--tw-rotate-y)) rotateZ(var(--tw-rotate-z)) 
+scale3d(var(--tw-scale-x), var(--tw-scale-y), var(--tw-scale-z))
+skewX(var(--tw-skew-x)) skewY(var(--tw-skew-y))`;
+
 module.exports = {
 	content: ['./index.html', './pages/*.{html,js}', './scripts/*.js'],
 	theme: {
+		backface: {
+			visible: 'visible',
+			hidden: 'hidden',
+			inherit: 'inherit',
+			initial: 'initial',
+			revert: 'revert',
+			'revert-layer': 'revert-layer',
+			unset: 'unset',
+		},
 		extend: {
 			colors: {
 				navy: {
@@ -17,7 +32,157 @@ module.exports = {
 					900: '#001c40',
 				},
 			},
+			zIndex: {
+				1: '1',
+				2: '2',
+				3: '3',
+				4: '4',
+				5: '5',
+				6: '6',
+				7: '7',
+				8: '8',
+				9: '9',
+				inf: '1000000',
+			},
 		},
 	},
-	plugins: [],
+	corePlugins: {
+		translate: false,
+		rotate: false,
+		scale: false,
+	},
+	plugins: [
+		// Default Values
+		function ({ addBase }) {
+			const base = { '--tw-translate-x': '0', '--tw-translate-y': '0', '--tw-translate-z': '0', '--tw-rotate-x': '0', '--tw-rotate-y': '0', '--tw-rotate-z': '0', '--tw-scale-x': '1', '--tw-scale-y': '1', '--tw-scale-z': '1' };
+			addBase({ '::backdrop': base });
+			addBase({ '*, ::before, ::after': base });
+		},
+
+		// child-*
+		function ({ addVariant, e }) {
+			addVariant('child', '& > *');
+			addVariant('child-first', '& > *:first-child');
+			addVariant('child-last', '& > *:last-child');
+			addVariant('child-even', '& > *:nth-child(even)');
+			addVariant('child-odd', '& > *:nth-child(odd)');
+		},
+
+		// 3D: Translate | Rotate | Scale
+		function ({ matchUtilities, theme }) {
+			// Translate XYZ
+			matchUtilities(
+				{
+					'translate-x': (value) => ({
+						'--tw-translate-x': value,
+						transform,
+					}),
+				},
+				{ values: theme('translate'), supportsNegativeValues: true }
+			);
+			matchUtilities(
+				{
+					'translate-y': (value) => ({
+						'--tw-translate-y': value,
+						transform,
+					}),
+				},
+				{ values: theme('translate'), supportsNegativeValues: true }
+			);
+			matchUtilities(
+				{
+					'translate-z': (value) => ({
+						'--tw-translate-z': value,
+						transform,
+					}),
+				},
+				{ values: theme('translate'), supportsNegativeValues: true }
+			);
+			// Rotate XYZ
+			matchUtilities(
+				{
+					'rotate-x': (value) => ({
+						'--tw-rotate-x': value,
+						transform,
+					}),
+				},
+				{ values: theme('rotate'), supportsNegativeValues: true }
+			);
+			matchUtilities(
+				{
+					'rotate-y': (value) => ({
+						'--tw-rotate-y': value,
+						transform,
+					}),
+				},
+				{ values: theme('rotate'), supportsNegativeValues: true }
+			);
+			matchUtilities(
+				{
+					'rotate-z': (value) => ({
+						'--tw-rotate-z': value,
+						transform,
+					}),
+				},
+				{ values: theme('rotate'), supportsNegativeValues: true }
+			);
+			// Scale XYZ
+			matchUtilities(
+				{
+					'scale-x': (value) => ({
+						'--tw-scale-x': value,
+						transform,
+					}),
+				},
+				{ values: theme('scale'), supportsNegativeValues: true }
+			);
+			matchUtilities(
+				{
+					'scale-y': (value) => ({
+						'--tw-scale-y': value,
+						transform,
+					}),
+				},
+				{ values: theme('scale'), supportsNegativeValues: true }
+			);
+			matchUtilities(
+				{
+					'scale-z': (value) => ({
+						'--tw-scale-z': value,
+						transform,
+					}),
+				},
+				{ values: theme('scale'), supportsNegativeValues: true }
+			);
+		},
+
+		// Randoms
+		function ({ matchUtilities, theme }) {
+			// Backface
+			matchUtilities(
+				{
+					backface: (value) => ({
+						'backface-visibility': value,
+					}),
+				},
+				{ values: theme('backface') }
+			);
+		},
+
+		function ({ addUtilities }) {
+			// transform-3d
+			addUtilities({
+				'.transform-3d': {
+					transform,
+				},
+			});
+
+			// preserve-3d
+			addUtilities({
+				'.preserve-3d': {
+					'transform-style': 'preserve-3d',
+				},
+			});
+		},
+	],
 };
