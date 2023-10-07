79a4787abb966bd40e4d123dae965519d23e15a5
[MΛLTE] <maltem.hilpert@gmx.de>
2023-10-07 11:11:15 +0200

    new global functions (set/remove: Url parameter), (copy to clipboard)

--- scripts/index.js
+++ scripts/index.js
@@ -2,11 +2,46 @@
 window.sleep = (time) => {
 	return new Promise((resolve) => setTimeout(resolve, time));
 };
+
 window.getUrlParams = (search = undefined) => {
 	let params = {};
 	decodeURIComponent(window.location.search).replace(/[?&]+([^=&]+)=([^&]*)/g, (m, key, value) => (params[key] = value));
 	return search ? params[search] : params;
 };
+window.setUrlParam = (key, value, pushState = false, replaceOld = false) => {
+	let current = replaceOld ? [] : window.getUrlParams();
+	current[key] = value;
+
+	let newParams = '';
+	Object.keys(current).forEach((key) => {
+		let prefix = newParams == '' ? '?' : '&';
+		newParams += prefix + key + '=' + current[key];
+	});
+	if (pushState) history.pushState(null, '', newParams);
+	else history.replaceState(null, '', newParams);
+};
+window.removeUrlParam = (key, pushState = false) => {
+	let current = window.getUrlParams();
+	delete current[key];
+
+	let newParams = '';
+	Object.keys(current).forEach((key) => {
+		let prefix = newParams == '' ? '?' : '&';
+		newParams += prefix + key + '=' + current[key];
+	});
+	if (pushState) history.pushState(null, '', newParams);
+	else history.replaceState(null, '', newParams);
+};
+
+window.copyToClipboard = (input, isElement = false) => {
+	var $temp = $('<input>');
+	$('body').append($temp);
+	if (isElement) $temp.val($(input).text()).select();
+	else $temp.val(input).select();
+	document.execCommand('copy');
+	$temp.remove();
+};
+
 window.templates = {};
 window.isMobile = window.matchMedia('only screen and (max-width: 760px)').matches;
 
