81f02f1d4829f3fb9b591e80d6086fc12633877c
[MΛLTE] <maltem.hilpert@gmx.de>
2023-10-07 11:10:26 +0200

    Added window. for global vars

--- scripts/fluentui-emoji.js
+++ scripts/fluentui-emoji.js
@@ -1,9 +1,9 @@
-// Add this script to your website:  			<script src="https://cdn.jsdelivr.net/gh/malte9799/malte9799.github.io/scripts/fluentui-emoji.js"></script>
+// Add this script to your website:  			<script src="https://cdn.jsdelivr.net/gh/malte9799/cdn/fluentui-emoji/fluentui-emoji.js"></script>
 // Add a img tag with the fluentui attribute:   <img fluentui="{emoji_name}">
 // Let the script do the rest
 // Full list can be found at: 					https://malte9799.github.io/?page=icons
 
-var img_not_found = 'https://cdn.jsdelivr.net/gh/malte9799/cdn/fluentui-emoji/not_found.png';
+// var img_not_found = 'https://cdn.jsdelivr.net/gh/malte9799/cdn/fluentui-emoji/not_found.png';
 
 function iconToUrl(icon) {
 	icon = icon.replace('pinata', 'piñata');
@@ -33,6 +33,6 @@ document.querySelectorAll('img').forEach((img) => {
 	if (!img.hasAttribute('fluentui')) return;
 	let image = new Image();
 	image.onload = () => (img.src = image.src);
-	image.onerror = () => (img.src = img_not_found);
+	image.onerror = () => (img.src = window.img_not_found);
 	image.src = iconToUrl(img.getAttribute('fluentui'));
 });
