1625ba0bf6a98c6c25ef8dab1aba5c655020cdc5
[MΛLTE] <maltem.hilpert@gmx.de>
2023-10-10 23:54:46 +0200

    new class order

--- pages/icons.js
+++ pages/icons.js
@@ -1,56 +1,51 @@
 window.templates.icon_card = `
 <article class="overflow-hidden">
-	<button class="flex flex-col items-center w-full h-full bg-gray-800 rounded-lg">
-		<img loading="lazy" class="w-3/4"/>
-		<span class="box-border inline-flex items-center m-1 mt-0 overflow-hidden text-xs leading-4 text-gray-300 break-words pointer-events-none min-h-[2rem] line-clamp-2 text-ellipsis"></span>
-	</button>
+  <button class="flex h-full w-full flex-col items-center rounded-lg bg-gray-800">
+    <img loading="lazy" class="w-3/4" />
+    <span class="pointer-events-none m-1 mt-0 box-border line-clamp-2 inline-flex min-h-[2rem] items-center overflow-hidden text-ellipsis break-words text-xs leading-4 text-gray-300"></span>
+  </button>
 </article>
 `;
 
 window.templates.icon_detail = `
-<div class="fixed inset-0 w-screen overflow-y-auto bg-gray-800 bg-opacity-50 z-.1 event">
-   <div id="modal" class="flex items-start justify-center min-h-full text-center ">
-      <div class="w-full p-8 mx-6 my-8 text-left transition-all transform bg-gray-900 shadow-xl rounded-xl max-w-7xl md:w-[80vw] md:mx-auto">
-         <div>
-            <button id="icon_name" class="font-bold text-white transition-colors text-[2rem] hover:text-blue-400 group" aria-label="Copy Icon Name" data-balloon-pos="up"></button>
-         </div>
-         <div class="flex flex-row flex-wrap gap-8 mt-12">
-            <div class="basis-full xl:basis-0 grow-0 xl:grow">
-               <div class="flex-col bg-[#183153] rounded-2xl min-h-[3em]">
-                  <div class="relative z-10 flex flex-col items-center justify-center py-8 overflow-hidden min-h-[20rem]">
-                     <img id="preview" class="flex pointer-events-none grow"/>
-                  </div>
-               </div>
-            </div>
-            <div class="basis-full xl:basis-0 grow-0 xl:grow">
-               <div id="selector" class="flex flex-col justify-center h-full gap-4 md:gap-8 max-h-[20rem]">
-               </div>
-            </div>
-         </div>
-         <div id="house" class="pt-12 mt-8 text-start">
-            <div>
-               <div role="tablist" class="flex flex-row text-sm font-bold text-white flex-nowrap overflow-clip bg-slate-800 rounded-t-md">
-                  <button role="tab" class="flex flex-row items-center px-5 py-3 bg-[#183153]">
-                  HTML
-                  </button>
-                  <button role="tab" class="flex flex-row items-center invisible px-5 py-3">
-                  SVG
-                  </button>
-               </div>
-            </div>
-            <div>
-               <button role="tabpanel" id="" class="p-0 bg-[#183153] rounded-b-md w-full text-start [--balloon-color:#146ebe]"  aria-label="Copy Code Snippet" data-balloon-pos="up">
-                	<div class="p-10" id="code"></div>
-               </button>
-            </div>
-         </div>
+<div class="z-.1 event fixed inset-0 w-screen overflow-y-auto bg-gray-800 bg-opacity-50">
+  <div id="modal" class="flex min-h-full items-start justify-center text-center">
+    <div class="mx-6 my-8 w-full max-w-7xl transform rounded-xl bg-gray-900 p-8 text-left shadow-xl transition-all md:mx-auto md:w-[80vw]">
+      <div>
+        <button id="icon_name" class="group text-[2rem] font-bold text-white transition-colors hover:text-blue-400" aria-label="Copy Icon Name" data-balloon-pos="up"></button>
       </div>
-   </div>
+      <div class="mt-12 flex flex-row flex-wrap gap-8">
+        <div class="grow-0 basis-full xl:grow xl:basis-0">
+          <div class="min-h-[3em] flex-col rounded-2xl bg-[#183153]">
+            <div class="relative z-10 flex min-h-[20rem] flex-col items-center justify-center overflow-hidden py-8">
+              <img id="preview" class="pointer-events-none flex grow" />
+            </div>
+          </div>
+        </div>
+        <div class="grow-0 basis-full xl:grow xl:basis-0">
+          <div id="selector" class="flex h-full max-h-[20rem] flex-col justify-center gap-4 md:gap-8"></div>
+        </div>
+      </div>
+      <div id="house" class="mt-8 pt-12 text-start">
+        <div>
+          <div role="tablist" class="flex flex-row flex-nowrap overflow-clip rounded-t-md bg-slate-800 text-sm font-bold text-white">
+            <button role="tab" class="flex flex-row items-center bg-[#183153] px-5 py-3">HTML</button>
+            <button role="tab" class="invisible flex flex-row items-center px-5 py-3">SVG</button>
+          </div>
+        </div>
+        <div>
+          <button role="tabpanel" id="" class="w-full rounded-b-md bg-[#183153] p-0 text-start [--balloon-color:#146ebe]" aria-label="Copy Code Snippet" data-balloon-pos="up">
+            <div class="p-10" id="code"></div>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
 `;
 
-window.templates.icon_detail_selector = `<div class="flex flex-row h-full max-h-[50%] min-h-[1rem] child-first:rounded-l-2xl child-first:border-l-4 child-last:rounded-r-2xl child-last:border-r-4"></div>`;
-window.templates.icon_detail_selector_img = `<button class="flex items-center justify-center flex-1 w-full transition-colors border-gray-800 hover:bg-gray-800 border-x-2 border-y-4 grow"><img class="max-h-full" width="100vh"/></button>`;
+window.templates.icon_detail_selector = `<div class="child-first:rounded-l-2xl child-first:border-l-4 child-last:rounded-r-2xl child-last:border-r-4 flex h-full max-h-[50%] min-h-[1rem] flex-row"></div>`;
+window.templates.icon_detail_selector_img = `<button class="flex w-full flex-1 grow items-center justify-center border-x-2 border-y-4 border-gray-800 transition-colors hover:bg-gray-800"><img class="max-h-full" width="100vh" /></button>`;
 
 var icons_per_page = 200;
 var icon_data;
@@ -126,7 +121,7 @@ function show_details(icon) {
 	let selector = $($.parseHTML(window.templates.icon_detail_selector.trim()));
 	let img = $($.parseHTML(window.templates.icon_detail_selector_img.trim()));
 
-	temp.find('#icon_name').html(`${icon}<span id="icon_style" class="text-lg text-gray-500 transition-colors pointer-events-none group-hover:text-blue-400">_${icon_style}</span>`);
+	temp.find('#icon_name').html(`${icon}<span id="icon_style" class="pointer-events-none text-lg text-gray-500 transition-colors group-hover:text-blue-400">_${icon_style}</span>`);
 	temp.find('#icon_name').on('click', (e) => {
 		window.copyToClipboard(window.getUrlParams('view').replace('-', '_'));
 		$(e.target).attr('aria-label', 'Copied');
